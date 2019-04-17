146776e3493ce747220c59dc4408bab4d4b1e49e
Pascal Helmer <pasc.ehlk@gmail.com>
2019-04-17 20:00:13 +0200

    Initial state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => (
+  <Amount
+    renderAmountOne={amount => (
+      <div>
+        <h1>My one Amount</h1>
+        <Pound amount={amount} />
+        <Euro amount={amount} />
+      </div>
+    )}
+    renderAmountTwo={amount => (
+      <div>
+        <h1>My other Amount</h1>
+        <Pound amount={amount} />
+        <Euro amount={amount} />
+      </div>
+    )}
+  />
+);
+
+class Amount extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      amount: 0
+    };
+  }
+
+  onIncrement = () => {
+    this.setState(state => ({ amount: state.amount + 1}));
+  };
+
+  onDecrement = () => {
+    this.setState(state => ({ amount: state.amount - 1 }));
+  }
+
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <span>US Dollar: {this.state.amount} </span>
+
+        {this.props.renderAmountTwo(this.state.amount)}
+
+        <button type="button" onClick={this.onIncrement}>
+          +
+        </button>
+        <button type="button" onClick={this.onDecrement}>
+          -
+        </button>
+
+        {this.props.renderAmountOne(this.state.amount)}
       </div>
     );
   }
 }
+
+const Euro = ({ amount }) => <p>Euro: {amount * 0.86}</p>;
+const Pound = ({ amount }) => <p>Pound: {amount * 0.76}</p>;
 
 export default App;
